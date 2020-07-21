a645037889226ea2cc942ceb6e18be5c98fc739b
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2020-07-21 16:52:20 +0530

    Update Bit_strings.cpp

--- CSES/Bit_strings.cpp
+++ CSES/Bit_strings.cpp
@@ -1,16 +1,14 @@
-/*
-https://cses.fi/problemset/task/1617
-*/
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(void)
 {    
-    long long int n,ans=1;
+    long long int n,a=1;
     cin>>n;
     for(int i=1;i<=n;i++)
     {
-        ans=(ans*2)%1000000007;
+        a=(a*2)%1000000007;
     }
-    cout<<ans<<endl;
+    cout<<a<<endl;
 }
  
