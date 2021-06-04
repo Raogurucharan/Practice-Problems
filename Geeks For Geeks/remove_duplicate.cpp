752c7cf66a79dd52097a888910aff4eaff770659
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-06-04 21:34:37 +0530

    Update remove_duplicate.cpp

--- Geeks For Geeks/remove_duplicate.cpp
+++ Geeks For Geeks/remove_duplicate.cpp
@@ -1,15 +1,12 @@
 /*
 Approach: The basic idea is to use a HashMap to solve the problem. But there is a catch, the numbers in the array are from 0 to n-1,
 and the input array has length n. So, the input array can be used as a HashMap. While Traversing the array,
-if an element ëaí is encountered then increase the value of a%nëth element by n. The frequency can be retrieved by dividing the a % níth element by n.
+if an element ‚Äòa‚Äô is encountered then increase the value of a%n‚Äòth element by n. The frequency can be retrieved by dividing the a % n‚Äôth element by n.
 Algorithm: 
 Traverse the given array from start to end.
-For every element in the array increment the arr[i]%nëth element by n.
+For every element in the array increment the arr[i]%n‚Äòth element by n.
 Now traverse the array again and print all those indexes i for which arr[i]/n is greater than 1. Which guarantees that the number n has been added to that index
-This approach works because all elements are in the range from 0 to n-1 and arr[i] would be greater than n only if a value ìiî has appeared more than once.*/
-
-// C++ code to find
-// duplicates in O(n) time
+This approach works because all elements are in the range from 0 to n-1 and arr[i] would be greater than n only if a value ‚Äúi‚Äù has appeared more than once.*/
 
 #include<iostream>
 using namespace std;
@@ -30,33 +27,3 @@ int main()
 	}
 	return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
