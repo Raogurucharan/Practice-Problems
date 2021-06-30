b2758f57846cbb6a4dd02e772f67f35544c25ea1
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-06-30 20:23:08 +0530

    Update fairplay.cpp

--- fairplay.cpp
+++ fairplay.cpp
@@ -1,15 +1,4 @@
-/*
-Four players participate in the playoff tournament. The tournament is held according to the following scheme: the first player will play with the second,
-and the third player with the fourth, then the winners of the pairs will play in the finals of the tournament.
-It is known that in a match between two players, the one whose skill is greater will win. The skill of the i-th player is equal to si and all skill levels are pairwise different
-(i. e. there are no two identical values in the array s).
-The tournament is called fair if the two players with the highest skills meet in the finals.
-Determine whether the given tournament is fair.
-Input
-The first line contains a single integer t (1≤t≤104) — the number of test cases.
-A single line of test case contains four integers s1,s2,s3,s4 (1≤si≤100) — skill of the players. It is guaranteed that all the numbers in the array are different.
-Output
-For each testcase, output YES if the tournament is fair, or NO otherwise.*/
+/* A. Fair Playoff [Codeforces] */
 
 #include <bits/stdc++.h>
  #include<iostream>
