3080c4276dbbcfbb31fe29742ea649db7d2f625d
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-07-11 15:18:00 +0530

    Update codejam2018.cpp

--- codejam2018.cpp
+++ codejam2018.cpp
@@ -4,8 +4,8 @@ This problem was asked in Google CodeJam 2019 online qualifier round.
 
 Problem Statement:
 
-Someone just won the Code Jam lottery, and we owe them N jamcoins! However, when we tried to print out an oversized check, we encountered a problem. The value of N, which is an integer,
- includes at least one digit that is a 4... and the 4 key on the keyboard of our oversized check printer is broken.
+Someone just won the Code Jam lottery, and we owe them N jamcoins! However, when we tried to print out an oversized check, we encountered a problem.
+The value of N, which is an integer, includes at least one digit that is a 4... and the 4 key on the keyboard of our oversized check printer is broken.
 
 Fortunately, we have a workaround: we will send our winner two checks for positive integer amounts A and B, such that neither A nor B contains any digit that is a 4, and A + B = N.
  Please help us find any pair of values A and B that satisfy these conditions.
