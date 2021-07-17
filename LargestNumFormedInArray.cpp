07f5eb8167fdac2627f10658ab8ffd70bd2bed98
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-07-17 16:27:23 +0530

    Add files via upload

---
+++ LargestNumFormedInArray.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+bool comparator(string first,string second)
+{
+    string one = first+second;
+    string two = second+first;
+    
+    int i=0;
+    while(one[i] && two[i])
+    {
+        if(one[i]>two[i])
+            return true;
+        else if(one[i]<two[i])
+            return false;
+        ++i;
+    }
+    
+    return false;
+}
+
+int main() {
+	//code
+	int tc;
+	cin>>tc;
+	while(tc--)
+	{
+	   int len;
+	   vector<string> arr;
+	   
+	   //Take Array inputs
+	   int i;
+	   string temp;
+	   cin>>len;
+	   for(i=0;i<len;++i)
+	   {
+	       cin>>temp;
+	       arr.push_back(temp);
+	   }
+	   sort(arr.begin(),arr.end(),comparator);
+	    
+	   for(i=0;i<len;++i)
+	       cout<<arr[i];
+	   cout<<"\n";
+	}
+	return 0;
+}
+//Time complexity: O(nlogn * len(longest string))
