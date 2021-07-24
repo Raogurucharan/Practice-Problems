49f86c003fecab3134bf4063102b4acf47f21401
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-07-24 22:24:03 +0530

    Update SubarraySumZero.cpp

--- SubarraySumZero.cpp
+++ SubarraySumZero.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 bool isZeroSub(int arr[],int n)
 {
-    unordered_set<int> set;       // create an empty set to store the sum of elements of each
-	                              // subarray `A[0…i]`, where `0 <= i < n`
+    unordered_set<int> set;                     // create an empty set to store the sum of elements of each
+	                                       // subarray `A[0…i]`, where `0 <= i < n`
     int sum=0;
-    set.insert(0);              // insert 0 into the set to handle the case when subarray with
-	                            // zero-sum starts from index 0
+    set.insert(0);             		      // insert 0 into the set to handle the case when subarray with
+	                                      // zero-sum starts from index 0
     for(int i=0;i<n;i++)
     {
         sum+=arr[i];
-        if(set.find(sum) != set.end())       // if the sum is seen before, we have found a subarray with zero-sum
+        if(set.find(sum) != set.end())        // if the sum is seen before, we have found a subarray with zero-sum
         {
             return true;
         }
@@ -30,3 +30,7 @@ int main()
     cout<<"Subarray with zero present":
     cout<<"subarray with zero absent";
 }
+
+
+//  Time complexity: O(n)
+//  Space complexity: O(n)
