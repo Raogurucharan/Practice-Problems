9e8059873fcaaf5e8dc786465f6e2b3650abd3f4
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-09-09 15:06:12 +0530

    Add files via upload

---
+++ MinProduct.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int minProduct(int A[], int n) {
+    int r = A[0];
+    int imax = r, imin = r;
+    for (int i = 1; i < n; i++) {     
+        if (A[i] < 0){
+            swap(imax, imin);
+        }
+        imax = max(A[i], imax * A[i]);
+        imin = min(A[i], imin * A[i]);
+        r = min(r, imin);
+    }
+    return r;
+}
+// Time complexity O(N)
+
+int main()
+{
+int a[] = {3,1,-9,-6,3};
+int n = sizeof(a)/sizeof(a[0]);
+int min_Prod = minProduct(a, n);
+cout << "Minimum contiguous Product is : " << min_Prod;
+return 0;
+}
