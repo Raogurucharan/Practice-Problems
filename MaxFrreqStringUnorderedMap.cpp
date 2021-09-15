758d44e88bc8613ccf7dd79d0d83021ed38560d4
Gurucharan rao <54024246+Raogurucharan@users.noreply.github.com>
2021-09-15 15:10:22 +0530

    Add files via upload

---
+++ MaxFrreqStringUnorderedMap.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+void countFreq(string str, int n)
+{
+    int arr[n];
+    int finalchar;
+     int element=0;
+	unordered_map<char, int> mp;
+	for (int i = 0; i < n; i++)
+		mp[str[i]]++;
+
+	for (auto x : mp)
+    {
+        if(x.second >element){
+				finalchar=x.first;
+				element=x.second;
+		}
+		cout << x.first << " " << x.second << endl;
+    }
+    cout<<"The max frequency is "<<element<<" and Element is "<<char(finalchar)<<endl;
+}
+int main()
+{
+	string str;
+    cin>>str;
+    int n=str.length();
+	countFreq( str , n);
+	return 0;
+}
